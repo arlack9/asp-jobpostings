3dea5c031b6da2c4de5186ccd4b12f2a7d1f4277
arlack9 <alarjun504@gmail.com>
2025-06-23 18:02:34 +0530

    go

--- DevSpot.Tests/obj/Debug/net9.0/DevSpot.Tests.AssemblyInfo.cs
+++ DevSpot.Tests/obj/Debug/net9.0/DevSpot.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevSpot.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a49b4e2d6a64f600cfbff5cbb16f7eaa315bd9c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a6b8815f56d14e7360078c0b80a975b595e610")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevSpot.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevSpot.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
