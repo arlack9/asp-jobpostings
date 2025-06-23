f1a6b8815f56d14e7360078c0b80a975b595e610
arlack9 <alarjun504@gmail.com>
2025-06-23 17:46:03 +0530

    go3

---
+++ DevSpot.Tests/JobPostingsRepositoryTests.cs
@@ -0,0 +1,63 @@
+﻿using DevSpot.Data;
+using DevSpot.Models;
+using DevSpot.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSpot.Tests
+{
+    public class JobPostingsRepositoryTests
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _options;
+
+        public JobPostingsRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("JobPostingDb") //in -memory db for testing
+                .Options;
+        }
+
+        private ApplicationDbContext CreateDbContext() => new ApplicationDbContext(_options);
+
+        [Fact]
+        public async Task AddAsync_ShouldAddJobPosting()
+        {
+            //db context
+            var db=CreateDbContext();
+            
+            //job posting repository 
+            var repository = new JobPostingRepository(db);
+            
+            //job posting
+            var jobPosting = new JobPosting
+            {
+                Title = "Test Title",
+                Description = "TEst Descirption",
+                PostedDate = DateTime.Now,
+                Company = "Test company",
+                Location = "Test Location",
+                UserId = "1234"
+            };
+
+            //execute
+            await repository.AddAsync(jobPosting);
+            
+            //result
+            var result = db.JobPostings.SingleOrDefault(x => x.Title == "Test Title");
+
+            //assert
+            Assert.NotNull(result);
+
+            Assert.Equal("Test Description", result.Title);
+
+
+
+
+
+        }
+    }
+}
